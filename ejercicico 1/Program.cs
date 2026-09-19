11c3b8e42aeb1a38d519820931137867459e1178
jonnathan <jonnathan@DESKTOP-K3MV004>
2026-09-19 02:57:56 -0600

    Agregar archivos de proyecto.

---
+++ ejercicico 1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("EJERCICIO_1");                                    //Numero de la practica
+Console.WriteLine("CÁLCULO DEL PROMEDIO DE TRES CALIFICACIONES");    //Nombre del ejercicio
+Console.WriteLine();                                                 //Instruccion para dejar un espacio en blanco en la consola
+
+Console.Write("Ingrese la primera calificación: ");
+double calificacion1 = Convert.ToDouble(Console.ReadLine());         //Declaramos la variable 1
+
+Console.Write("Ingrese la segunda calificación: ");
+double calificacion2 = Convert.ToDouble(Console.ReadLine());         //Declaramos la variable 2
+
+Console.Write("Ingrese la tercera calificación: ");
+double calificacion3 = Convert.ToDouble(Console.ReadLine());         //Declaramos la variable 3 
+
+double promedio = (calificacion1 + calificacion2 + calificacion3) / 3.0;
+
+Console.WriteLine();
+Console.WriteLine($"El promedio de las tres calificaciones es: {promedio:F2}");
